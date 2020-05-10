d454be14cd7ff14e8c36f29df84994a83a513e28
francijpg <hellofranciscopg@gmail.com>
2020-05-10 15:22:22 -0400

    config.js created for the project architecture and environment variable management

---
+++ backend/config/config.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv')
+
+if (process.env.NODE_ENV === "production"){
+  dotenv.config({ path: __dirname + '/../../.env' })
+}
+if (process.env.NODE_ENV === "test"){
+  dotenv.config({ path: __dirname + '/../../test.env' })
+}
+if (process.env.NODE_ENV === "development"){
+  dotenv.config({ path: __dirname + '/../../development.env' })
+}
+
+const config = {
+  appConfig: {
+    port: process.env.BACKEND_PORT,
+    version: process.env.BACKEND_VERSION,
+    environment: process.env.NODE_ENV
+  },
+  dbConfig: {
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    dbName: process.env.DB_NAME
+  },
+  apis: {
+    url_place_holder: process.env.API_JSON_PLACE_HOLDER
+  }
+}
+
+module.exports = config
