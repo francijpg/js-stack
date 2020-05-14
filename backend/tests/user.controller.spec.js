a7b24d222869da97c99063054749dfffdc2e7eb8
francijpg <hellofranciscopg@gmail.com>
2020-05-14 12:16:03 -0400

    unit test with jest

---
+++ backend/tests/user.controller.spec.js
@@ -0,0 +1,27 @@
+const handlers = require('../controllers/users.controller')
+
+describe('controllers', () => {
+    describe('users.controllers', () => {
+        describe('get', () => {
+            it('returns a list of users in json format', async () => {
+                const axios = {
+                    get: jest.fn().mockResolvedValue({ data: 1})
+                }
+                const api = jest.fn()
+                const res = {
+                    status: jest.fn().mockReturnThis(),
+                    send: jest.fn()
+                }
+                
+                await handlers({ axios }, api).get({}, res)
+
+                expect(res.status.mock.calls).toEqual([
+                    [200]
+                ])
+                expect(res.send.mock.calls).toEqual([
+                    [1]
+                ])
+            })
+        })
+    })
+})
