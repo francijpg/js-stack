6562a58a739b7bf042d1b1b4c5c8245560d3d334
francijpg <hellofranciscopg@gmail.com>
2020-05-17 13:41:21 -0400

    react-router-dom dependency added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Routes from './components/Routes'
 
 function App() {
   return (
     <div className="App">
+      <Routes/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
