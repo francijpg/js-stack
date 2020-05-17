7aa609289752c478891ad0565c6614efbb3d3520
francijpg <hellofranciscopg@gmail.com>
2020-05-17 14:26:33 -0400

    Create home.js page

--- frontend/src/components/Routes.js
+++ frontend/src/components/Routes.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom'
+import Home from '../pages/Home'
 
 export default function Routes() {
     return (
-        <div>
-            <h1>RUTA</h1>
-        </div>
+        <Router>
+            <header>
+                <nav>
+                    <Link to="/" className="App-link">
+                        Home
+                    </Link>
+                </nav>
+            </header>
+            <Switch>
+                <Route exact path="/" component={Home}></Route>
+            </Switch>
+        </Router>
     )
 }
